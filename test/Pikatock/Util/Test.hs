6b41d489e65f8a98cf5d3d6b7db8e1eac8538bc4
sjc <s.cole.j@gmail.com>
2017-01-28 09:24:28 +1100

    Hindent all the things.

--- test/Pikatock/Util/Test.hs
+++ test/Pikatock/Util/Test.hs
@@ -1,4 +1,6 @@
-module Pikatock.Util.Test (tests) where
+module Pikatock.Util.Test
+  ( tests
+  ) where
 
 import Data.Time.Calendar
 import Data.Time.LocalTime
@@ -8,11 +10,11 @@ import Pikatock.Util
 import Test.HUnit
 
 tests :: Test
-tests = TestList $ concat [ findLastMondayTest
-                          , entryInWorkWeekTest
-                          ]
+tests = TestList $ concat [findLastMondayTest, entryInWorkWeekTest]
 
-findLastMondayTest = map TestCase
+findLastMondayTest =
+  map
+    TestCase
     [ findLastMonday (fromGregorian 2016 11 15) @=? (fromGregorian 2016 11 14)
     , findLastMonday (fromGregorian 2016 11 17) @=? (fromGregorian 2016 11 14)
     ]
@@ -20,9 +22,10 @@ findLastMondayTest = map TestCase
 entryInWorkWeekTest =
   let entryForDay d = Entry d midnight midnight []
       now = fromGregorian 2016 11 15
-  in map TestCase
-       [ entryInWorkWeek now (entryForDay $ fromGregorian 2016 11 15)  @=? True
-       , entryInWorkWeek now (entryForDay $ fromGregorian 2016 11 14)  @=? True
-       , entryInWorkWeek now (entryForDay $ fromGregorian 2016 11 13)  @=? False
-       , entryInWorkWeek now (entryForDay $ fromGregorian 2016 11 19)  @=? False
+  in map
+       TestCase
+       [ entryInWorkWeek now (entryForDay $ fromGregorian 2016 11 15) @=? True
+       , entryInWorkWeek now (entryForDay $ fromGregorian 2016 11 14) @=? True
+       , entryInWorkWeek now (entryForDay $ fromGregorian 2016 11 13) @=? False
+       , entryInWorkWeek now (entryForDay $ fromGregorian 2016 11 19) @=? False
        ]
--- test/TestMain.hs
+++ test/TestMain.hs
@@ -1,6 +1,6 @@
-import Test.HUnit
 import qualified Pikatock.Util.Test
 import Test.Framework
 import Test.Framework.Providers.HUnit
+import Test.HUnit
 
 main = defaultMain $ hUnitTestToTests Pikatock.Util.Test.tests
